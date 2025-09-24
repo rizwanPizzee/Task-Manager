9192d90380e68ff241ba37dbfacac8b8c3c66768
rizwanPizzee <rizwanpizzee@gmail.com>
2025-09-24 20:03:45 +0500

    Setup the SQLite database and created a model.

--- app.py
+++ app.py
@@ -1,6 +1,23 @@
 from flask import Flask, render_template, url_for
-
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 app = Flask(__name__)
+
+#where the database is located
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+
+#Database
+db = SQLAlchemy(app)
+
+#Model
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    content = db.Column(db.String(200), nullable = False)
+    completed = db.Column(db.Integer, default = 0)
+    date_created = db.Column(db.DateTime, default = datetime.utcnow)
+
+    def __repr__(self):
+        return f"<Task {self.id}>"
 
 @app.route("/")
 def hello_world():
@@ -8,4 +25,6 @@ def hello_world():
 
 
 if __name__ == "__main__":
+    with app.app_context():   
+        db.create_all()
     app.run(debug=True)
