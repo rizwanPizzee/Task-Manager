fadda624923fe9be588d78986f17aa2b096b8f6e
rizwanPizzee <rizwanpizzee@gmail.com>
2025-09-24 21:07:43 +0500

    Created the display for tasks and other functions related to it in index.html. in app.py todo function, wrote the code for the creation of task and then commit it to database.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 app = Flask(__name__)
@@ -19,9 +19,22 @@ class Todo(db.Model):
     def __repr__(self):
         return f"<Task {self.id}>"
 
-@app.route("/")
-def hello_world():
-    return render_template('index.html')
+@app.route("/", methods = ['GET', 'POST'])
+def todo():
+    if request.method == "POST":
+        task_content = request.form['content']
+        new_task = Todo(content = task_content)
+
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect('/')
+        except:
+            print('Error while adding task!')
+        
+    else:
+        tasks = Todo.query.order_by(Todo.date_created).all()
+        return render_template('index.html', tasks= tasks)
 
 
 if __name__ == "__main__":
